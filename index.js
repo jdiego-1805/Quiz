7126cc5b73328d43484685ed5fad5b8c40562708
jdiego-1805 <63172283+jdiego-1805@users.noreply.github.com>
2023-04-07 15:58:05 -0500

    added some questions

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+let timer = 90
+
+let timerId = setInterval(function(){
+
+    timer -=1
+    console.log(timer);
+
+}, 1000)
+
+
+let questions = [{question: "Which Pokemon region came out first?", answers:["Hoenn", "Unova", "Alola", "Johto"], correctAnswer: "Johto" },
+{question: "Which Pokemon is the god of all Pokemon?", answers:["Mewtwo", "Arceus", Regigigas", "Rayquaza"], correctAnswer: "Arceus" }
+
+]
